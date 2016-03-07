5f412b836a4dde5cdf9ae315c0c11edc56ccda62
Andy Fang <andyfang.dz@gmail.com>
2016-03-07 11:13:58 -0500

    Add login & logout link

--- js/components/pages/HomePage.react.js
+++ js/components/pages/HomePage.react.js
@@ -15,14 +15,8 @@ class HomePage extends Component {
     return (
       <div>
         <h1>Hello World!</h1>
-        <h2>This is the demo for the <span className="home__text--red">{ projectName }</span> by <a href={'https://twitter.com/' + ownerName} >@{ ownerName }</a></h2>
-        <label className="home__label">Change to your project name:
-          <input className="home__input" type="text" onChange={(evt) => { dispatch(asyncChangeProjectName(evt.target.value)); }} defaultValue="React.js Boilerplate" value={projectName} />
-        </label>
-        <label className="home__label">Change to your name:
-          <input className="home__input" type="text" onChange={(evt) => { dispatch(asyncChangeOwnerName(evt.target.value)); }} defaultValue="mxstbr" value={ownerName} />
-        </label>
-        <Link className="btn" to="/readme">Setup</Link>
+        <Link className="btn" to="/login">Login</Link> <br/>
+        <Link className="btn" to="/register">Register</Link>
       </div>
     );
   }
